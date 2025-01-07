7b544d52b28af0cfa9ccb06214fb5e72776eeb1e
ruqaiyae <ruqaiyaelectricwala@gmail.com>
2025-01-07 18:25:41 +0000

    Completed dom-manipulation exercise

---
+++ dom-manipulation/main.ts
@@ -0,0 +1,29 @@
+let clicks: number = 0;
+
+const $hotButton = document.querySelector('.hot-button');
+const $clickCount = document.querySelector('.click-count');
+
+if (!$hotButton) throw new Error('The $hotButton query failed');
+
+$hotButton.addEventListener('click', () => {
+  clicks++;
+  if (!$hotButton || !$clickCount) {
+    throw new Error('The $hotButton or $clickCount query failed');
+  }
+  const clicksToString = clicks.toString();
+  $clickCount.textContent = `Clicks: ${clicksToString}`;
+
+  if (clicks < 4) {
+    $hotButton.className = 'hot-button cold';
+  } else if (clicks < 7) {
+    $hotButton.className = 'hot-button cool';
+  } else if (clicks < 10) {
+    $hotButton.className = 'hot-button tepid';
+  } else if (clicks < 13) {
+    $hotButton.className = 'hot-button warm';
+  } else if (clicks < 16) {
+    $hotButton.className = 'hot-button hot';
+  } else {
+    $hotButton.className = 'hot-button nuclear';
+  }
+});
