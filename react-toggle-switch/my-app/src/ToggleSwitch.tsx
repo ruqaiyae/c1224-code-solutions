b904ec81c90602d644b5c7622efc101d413a0eff
ruqaiyae <ruqaiyaelectricwala@gmail.com>
2025-02-13 02:55:35 +0000

    Updated code to use ternary operators

--- react-toggle-switch/my-app/src/ToggleSwitch.tsx
+++ react-toggle-switch/my-app/src/ToggleSwitch.tsx
@@ -4,30 +4,14 @@ import './ToggleSwitch.css';
 export function ToggleSwitch() {
   const [isOn, setIsOn] = useState(false);
 
-  let switchOn;
-  let label;
-  if (isOn) {
-    switchOn = 'toggle-switch is-on';
-    label = 'ON';
-  } else {
-    switchOn = 'toggle-switch';
-    label = 'OFF';
-  }
-
-  function handleClick() {
-    if (isOn) {
-      setIsOn(false);
-    } else {
-      setIsOn(true);
-    }
-  }
-
   return (
-    <div onClick={handleClick} className={switchOn}>
+    <div
+      onClick={() => setIsOn(!isOn)}
+      className={`toggle-switch ${isOn ? 'is-on' : ''}`}>
       <div className="slider">
         <div className="switch"></div>
       </div>
-      <span className="state-label">{label}</span>
+      <span className="state-label">{isOn ? 'ON' : 'OFF'}</span>
     </div>
   );
 }
