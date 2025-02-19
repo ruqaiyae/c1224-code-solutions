50b4e143e2c51243b3bb36a8da0b231e6ce635f1
ruqaiyae <ruqaiyaelectricwala@gmail.com>
2025-02-19 19:11:05 +0000

    Removed unnecessary fragments

--- react-search/my-app/src/App.tsx
+++ react-search/my-app/src/App.tsx
@@ -1,11 +1,7 @@
 import { SearchableList } from './SearchableList';
 
 function App() {
-  return (
-    <>
-      <SearchableList />
-    </>
-  );
+  return <SearchableList />;
 }
 
 export default App;
