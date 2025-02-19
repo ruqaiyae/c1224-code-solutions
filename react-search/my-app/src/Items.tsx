2c5476278ccebf394fa7de02eb42d47f7e443982
ruqaiyae <ruqaiyaelectricwala@gmail.com>
2025-02-19 19:10:04 +0000

    Moved the if-statement before the for-loop

--- react-search/my-app/src/Items.tsx
+++ react-search/my-app/src/Items.tsx
@@ -3,13 +3,13 @@ type ItemsProps = {
 };
 
 export function Items({ filteredQuotes }: ItemsProps) {
+  if (filteredQuotes.length === 0) {
+    return <p>No items match the filter</p>;
+  }
+
   const items = [];
   for (let i = 0; i < filteredQuotes.length; i++) {
     items.push(<li key={i}> {filteredQuotes[i]} </li>);
-  }
-
-  if (filteredQuotes.length === 0) {
-    return <p>No items match the filter</p>;
   }
 
   return <ul>{items}</ul>;
