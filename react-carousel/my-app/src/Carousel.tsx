95b053bc5d779bffc61d0555c55787a62c071ee3
ruqaiyae <ruqaiyaelectricwala@gmail.com>
2025-02-19 18:54:41 +0000

    Completed react-carousel challenge

---
+++ react-carousel/my-app/src/Carousel.tsx
@@ -0,0 +1,91 @@
+import { useEffect, useState } from 'react';
+import {
+  FaAngleLeft,
+  FaAngleRight,
+  FaCircle,
+  FaRegCircle,
+} from 'react-icons/fa';
+import { type Image } from './App';
+import './Carousel.css';
+
+type Props = {
+  images: Image[];
+};
+
+export function Carousel({ images }: Props) {
+  const [imageIndex, setImageIndex] = useState(0);
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setImageIndex((imageIndex + 1) % images.length);
+    }, 1000);
+    return () => clearTimeout(timerId);
+  }, [setImageIndex, imageIndex, setTimeout, clearTimeout]);
+
+  return (
+    <>
+      <div className="container">
+        <Btn
+          prev="prev"
+          onBtnClick={() =>
+            setImageIndex((imageIndex - 1 + images.length) % images.length)
+          }
+        />
+
+        <ImageDisplay image={images[imageIndex]} />
+
+        <Btn
+          next="next"
+          onBtnClick={() => setImageIndex((imageIndex + 1) % images.length)}
+        />
+      </div>
+
+      <Dots
+        count={images.length}
+        imageIndex={imageIndex}
+        onDotClick={(i) => setImageIndex(i)}
+      />
+    </>
+  );
+}
+
+type BtnProps = {
+  prev?: string;
+  next?: string;
+  onBtnClick: () => void;
+};
+
+function Btn({ prev, next, onBtnClick }: BtnProps) {
+  if (prev) return <FaAngleLeft onClick={onBtnClick} className="btn" />;
+  else if (next) return <FaAngleRight onClick={onBtnClick} className="btn" />;
+}
+
+type ImageProp = {
+  image: Image;
+};
+
+function ImageDisplay({ image }: ImageProp) {
+  return (
+    <img src={image.src} alt={image.alt} key={image.alt} className="image" />
+  );
+}
+
+type dotsProps = {
+  count: number;
+  imageIndex: number;
+  onDotClick: (i: number) => void;
+};
+
+function Dots({ count, imageIndex, onDotClick }: dotsProps) {
+  const dots = [];
+  for (let i = 0; i < count; i++) {
+    dots.push(
+      i === imageIndex ? (
+        <FaCircle key={i} />
+      ) : (
+        <FaRegCircle key={i} onClick={() => onDotClick(i)} />
+      )
+    );
+  }
+  return <div className="dots-container">{dots}</div>;
+}
