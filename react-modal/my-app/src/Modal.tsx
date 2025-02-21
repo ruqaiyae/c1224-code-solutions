428f76ef9a88b3b3861d3f1820b67c8fc3c7251a
ruqaiyae <ruqaiyaelectricwala@gmail.com>
2025-02-21 20:02:40 +0000

    Completed react-modal exercise

---
+++ react-modal/my-app/src/Modal.tsx
@@ -0,0 +1,20 @@
+import { ReactNode, useEffect, useRef } from 'react';
+
+type Props = {
+  children: ReactNode;
+  isOpen: boolean;
+  onClose: () => void;
+};
+
+export function Modal({ children, isOpen, onClose }: Props) {
+  const modal = useRef<HTMLDialogElement>(null);
+  useEffect(() => {
+    isOpen ? modal.current?.showModal() : modal.current?.close();
+  }, [isOpen]);
+
+  return (
+    <dialog ref={modal} onClose={onClose}>
+      {children}
+    </dialog>
+  );
+}
