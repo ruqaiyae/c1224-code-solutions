93059039e4338c03d457b6bf1106ebc4a5eba1ed
ruqaiyae <ruqaiyaelectricwala@gmail.com>
2025-03-07 19:35:15 +0000

    Completed react-context exercise

---
+++ react-context/my-app/src/components/CartContext.tsx
@@ -0,0 +1,37 @@
+import { createContext, ReactNode, useState } from 'react';
+import { type Product } from '../lib';
+
+export type CartValue = {
+  cart: Product[];
+  addToCart: (product: Product) => void;
+};
+
+const defaultCartValue: CartValue = {
+  cart: [],
+  addToCart: () => undefined,
+};
+
+export const CartContext = createContext(defaultCartValue);
+
+type Props = {
+  children: ReactNode;
+};
+
+export function CartProvider({ children }: Props) {
+  const [cart, setCart] = useState<Product[]>([]);
+
+  function addToCart(product: Product) {
+    setCart([...cart, product]);
+  }
+
+  const cartContextValues = {
+    cart,
+    addToCart,
+  };
+
+  return (
+    <CartContext.Provider value={cartContextValues}>
+      {children}
+    </CartContext.Provider>
+  );
+}
